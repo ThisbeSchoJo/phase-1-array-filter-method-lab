5f9f349315abe62f0e9419ef03a3a202593743e2
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-08 15:36:38 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driverNames, name) {
+    return driverNames.filter((driver) => { //outer return shows test that you're returning value
+        return name.toLowerCase() === driver.toLowerCase() //inner return controls what result will be
+    })
+}
+
+function fuzzyMatch(driversNames, letters) {
+    return driversNames.filter((name) => { //use return statement so the test can check what we're returning
+        return name.slice(0,letters.length) === (letters)
+    })
+}
+
+function matchName(driverObjects, name){
+    return driverObjects.filter((driver) =>{
+        return driver["name"] === name
+    })
+}
